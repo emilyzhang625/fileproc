de6bde70c936f512b78ad2bac4eeb1c3f5ab4dcb
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-09-02 00:14:35 -0700

    more button stuff

--- frontend/src/Button.jsx
+++ frontend/src/Button.jsx
@@ -1,6 +1,23 @@
 function Button() {
-  const handleUp = () => {};
-  return <button onClick={handleUp}>File Upload</button>;
+  const handleUp = () => {
+    const file = document.getElementById("textFile").files[0];
+    console.log(file);
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const text = event.target.result;
+      console.log(text);
+    };
+
+    reader.readAsText(file);
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".txt" id="textFile"></input>
+      <button onClick={handleUp}>Process Text File</button>
+    </div>
+  );
 }
 
 export default Button;
